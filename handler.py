43ac14b3979df7a5296ffa3e8be9adcfc3c88e0f
Saatwik-Mehta <Saatwikmehta@gmail.com>
2025-02-26 13:56:28 +0530

    layer implementation

--- handler.py
+++ handler.py
@@ -1,9 +1,10 @@
 import json
 import requests
+from Utils.common.all_apis import sample_post
 
 def hello(event, context):
     body = {
         "message": "Go Serverless v4.0! Your function executed successfully!"
     }
-
+    print(sample_post("https://www.google.com"))
     return {"statusCode": 200, "body": json.dumps(body)}
